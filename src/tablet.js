53ea11a881b195d77c104f5f563711cee450fec8
Mitch Bradley <wmb@firmworks.com>
2024-06-20 18:34:40 -1000

    Sort file list

--- src/tablet.js
+++ src/tablet.js
@@ -2885,6 +2885,18 @@ function clearTabletFileSelector(message) {
 }
 
 function populateTabletFileSelector(files, path) {
+    files.sort((a, b) => {
+        const nameA = a.name.toUpperCase(); // ignore upper and lowercase
+        const nameB = b.name.toUpperCase(); // ignore upper and lowercase
+        if (nameA < nameB) {
+            return -1;
+        }
+        if (nameA > nameB) {
+             return 1;
+        }
+        // names must be equal
+        return 0;
+    });
     var selector = id('filelist');
 
     var selectedFile = gCodeFilename.split('/').slice(-1)[0];
